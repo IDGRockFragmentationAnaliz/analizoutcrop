fe361a8eb7da24b5705087b3d6563655e16b234d
koladik <koladik@gmail.com>
2025-07-01 18:20:44 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # outcropanaliz
+
+```commandline
+pip install numpy
+pip install pathlib
+pip install matplotlib
+pip install opencv-python
+pip install scipy
+pip install pyshp
+pip install pandas
+```
---
+++ main.py
@@ -0,0 +1,98 @@
+from pathlib import Path
+
+import numpy as np
+import cv2
+import scipy as sp
+import json
+import matplotlib.pyplot as plt
+
+
+from rocknetmanager.tools.image_data import ImageData
+from pyrocksegmentation.basic_segmentator import Segmentator
+from pyrockstats.distrebutions import lognorm, weibull, paretoexp
+from pyrockstats.bootstrap.ks_statistics import get_ks_distribution
+from pyrockstats.bootstrap.ks_statistics import get_confidence_value
+from pyrockstats.empirical import ecdf
+from distrebution_test import DistributionTest
+
+
+def main():
+    data_folder = Path("/media/koladik/HardDisk/data/OutcropData")
+    out_data = {}
+    with open("./data/config.json") as file:
+        config = json.load(file)
+
+    for i, image_folder in enumerate(data_folder.iterdir()):
+        print(image_folder.name)
+        data = get_data(image_folder, config)
+        
+        # fig = plt.figure(figsize=(12, 4))
+        # axs = [fig.add_subplot(1, 1, 1)]
+        # axs[0].plot(x, tests["lognorm"].model_cdf(x))
+        # axs[0].plot(data["ecdf"]["values"], data["ecdf"]["freqs"], color="black", linestyle="--", label="2")
+        # axs[0].set_xscale('log')
+        # plt.show()
+
+        out_data[image_folder.name] = data
+
+    with open("./data/outcrops_tests.json", 'w+') as json_file:
+        json.dump(out_data, json_file, indent=4)
+
+
+
+def get_data(image_folder, config):
+    name = image_folder.name
+
+    image_data, _ = ImageData.load(image_folder)
+
+    s = get_areas(image_data)
+    pix2m2 = (config[name]["m"] / config[name]["pix"]) ** 2
+
+    s = s[1:]
+    areas = s * pix2m2
+
+    xmin = np.min(areas)
+    xmax = np.max(areas)
+
+    models = {"lognorm": lognorm, "paretoexp": paretoexp, "weibull": weibull}
+    tests = {name: DistributionTest(areas, model) for name, model in models.items()}
+
+    ks_tests = {name: test.ks_test(0.05) for name, test in tests.items()}
+    thetas = {name: (float(test.theta[0]), float(test.theta[1])) for name, test in tests.items()}
+    values, e_freq = ecdf(areas)
+    x = np.logspace(np.log10(xmin), np.log10(xmax), 100)
+    bins = np.logspace(np.log10(xmin), np.log10(xmax), 10)
+    hist, bins = np.histogram(areas, bins)
+    hist = hist / (np.sum(s) * pix2m2)
+    alpha = 0.05
+    data = {
+        "x": x.tolist(),
+        "xmin": xmin,
+        "xmax": xmax,
+        "alpha": alpha,
+        "test_data": {name: test.get_data(x, alpha) for name, test in tests.items()},
+        "ecdf": {"values": values.tolist(), "freqs": e_freq.tolist()},
+        "theta": {name: tests[name].theta for name, test in tests.items()},
+        "bins": bins.tolist(),
+        "density": hist.tolist(),
+        "units": "m2"
+    }
+    return data
+
+
+def get_areas(image_data: ImageData):
+    label, _, _ = cv2.split(image_data.label)
+    area, _, _ = cv2.split(255 - image_data.area)
+    #
+    segments = Segmentator(label)
+    marks = segments.run()
+    marks_image = segments.get_segment_image()
+    cv2.imwrite("./data/image_3.png", marks_image)
+    unique, s = np.unique(marks, return_counts=True)
+
+    return s
+
+
+if __name__ == "__main__":
+    main()
+
